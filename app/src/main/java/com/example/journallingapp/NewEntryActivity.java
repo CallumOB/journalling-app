29d7587654f5abd2599577904bbcd62c9764e768
CallumOB <callumobrien2004@gmail.com>
2023-11-13 11:08:34 +0000

    new entry layout created

--- app/src/main/java/com/example/journallingapp/NewEntryActivity.java
+++ app/src/main/java/com/example/journallingapp/NewEntryActivity.java
@@ -1,4 +1,29 @@
 package com.example.journallingapp;
 
-public class NewEntryActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class NewEntryActivity extends AppCompatActivity {
+
+    private TextView prompt;
+    private TextView date;
+    private TextView location;
+    private EditText contents;
+    private Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_entry);
+
+        prompt = findViewById(R.id.newEntryPrompt);
+        date = findViewById(R.id.newEntryDate);
+        location = findViewById(R.id.newEntryLocation);
+        contents = findViewById(R.id.newEntryText);
+        submit = findViewById(R.id.newEntrySubmit);
+    }
 }
