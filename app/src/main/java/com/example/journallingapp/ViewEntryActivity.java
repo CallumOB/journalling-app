020d458e762f52537e90676a31a86e33c7affc37
CallumOB <callumobrien2004@gmail.com>
2023-11-14 21:20:00 +0000

    created ViewEntryActivity

--- app/src/main/java/com/example/journallingapp/ViewEntryActivity.java
+++ app/src/main/java/com/example/journallingapp/ViewEntryActivity.java
@@ -1,4 +1,14 @@
 package com.example.journallingapp;
 
-public class ViewEntryActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_entry);
+    }
 }
