f9c24cafe2b280ad99e89fe74ae1dd8b7d3ad46c
CallumOB <callumobrien2004@gmail.com>
2023-12-01 14:48:47 +0000

    added threads to NewEntryActivity

---
+++ app/src/main/java/com/example/journallingapp/BackgroundThread.java
@@ -0,0 +1,32 @@
+package com.example.journallingapp;
+
+import android.os.Handler;
+import android.os.Looper;
+
+public class BackgroundThread extends Thread {
+
+    /* This code is taken from lab 7 - progress bar. */
+
+    private Handler handler;
+
+    @Override
+    public void run() {
+        super.run();
+        Looper.prepare(); // Associating thread
+        handler = new Handler();
+        Looper.loop();
+    }
+
+    /**
+     * This method is used to add different tasks to the message queue
+     *
+     * @param task
+     */
+    public void addTaskToMessageQueue(Runnable task) {
+        handler.post(task); // First task1 is added to the queue then task2.
+    }
+
+    public Handler getHandler() {
+        return handler;
+    }
+}
